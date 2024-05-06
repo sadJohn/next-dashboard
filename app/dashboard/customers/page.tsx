a0332eedd9ce00f2bbd7dde05e6a14b703b10ba8
王天一 <1938159279@qq.com>
2024-05-06 14:39:20 +0800

    Setting up database

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,26 +1,3 @@
-import { fetchFilteredCustomers } from '@/app/lib/data';
-import CustomersTable from '@/app/ui/customers/table';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Customers',
-};
-
-export default async function Page({
-  searchParams,
-}: {
-  searchParams?: {
-    query?: string;
-    page?: string;
-  };
-}) {
-  const query = searchParams?.query || '';
-
-  const customers = await fetchFilteredCustomers(query);
-
-  return (
-    <main>
-      <CustomersTable customers={customers} />
-    </main>
-  );
+export default function Page() {
+  return <p>Customers Page</p>;
 }
